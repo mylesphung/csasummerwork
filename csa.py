fca4cdb0fc6456259b815569f15def25b4d386d9
MP <mylesphung03@gmail.com>
2021-06-29 13:53:12 -0400

    Create csa.py

---
+++ csa.py
@@ -0,0 +1,22 @@
+## assignment:
+## Your program should tell me 5 things about you. Things you would like me to know.
+## These could be your favorite things, interesting things about you, what you are
+## doing over the summer, an about you page, I believe statements, etc.
+## The program must include code and should have input and output.
+## (This should not be a static output. This is not a one page slide. )
+
+## imports
+import keyboard as kb
+
+def options():
+    pass
+
+def main():
+    print("Welcome to Myles' CSA Summer Work!!\n")
+    print("Press the space bar to start")
+    options()
+
+def options():
+    print("test")
+
+main()
